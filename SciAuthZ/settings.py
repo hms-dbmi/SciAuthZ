61a618b06ebf61367c177dae1229afc87d31f22c
mtmcduffie <mcduffie.michael@gmail.com>
2016-12-01 10:08:52 -0500

    Wrong encoding

--- SciAuthZ/settings.py
+++ SciAuthZ/settings.py
@@ -116,7 +116,7 @@ REST_FRAMEWORK = {
 }
 
 JWT_AUTH = {
-    'JWT_SECRET_KEY': base64.b64decode(os.environ.get("AUTH0_SECRET")),
+    'JWT_SECRET_KEY': base64.b64decode(os.environ.get("AUTH0_SECRET"), '-_'),
     'JWT_AUDIENCE': os.environ.get("AUTH0_CLIENT_ID"),
     'JWT_PAYLOAD_GET_USERNAME_HANDLER': 'authorization.permissions.jwt_get_username_from_payload'
 }
