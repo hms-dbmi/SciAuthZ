cbbab3ab511378c04f5c264e18815575ab82959e
mtmcduffie <mcduffie.michael@gmail.com>
2017-01-04 08:41:58 -0500

    TC-17 - Adding new users if they call SciAuthZ for the first time.

--- authorization/permissions.py
+++ authorization/permissions.py
@@ -1,5 +1,8 @@
 from rest_framework import permissions
+from django.contrib.auth.models import User
 
+import logging
+logger = logging.getLogger(__name__)
 
 class IsAssociatedUser(permissions.BasePermission):
     """
@@ -11,5 +14,18 @@ class IsAssociatedUser(permissions.BasePermission):
 
 
 def jwt_get_username_from_payload(payload):
+
+    logger.debug("Attempting to Authenticate User - " + payload.get('email'))
+
+    try:
+        User.objects.get(username=payload.get('email'))
+    except User.DoesNotExist:
+        logger.debug("User not found, creating.")
+
+        user = User(username=payload.get('email'), email=payload.get('email'))
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+
     return payload.get('email')
 
