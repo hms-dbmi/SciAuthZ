8aafcdf88a6cca0607356ccde6de61f7229368d1
mtmcduffie <mcduffie.michael@gmail.com>
2017-01-11 07:39:35 -0500

    PPM-45 - Updating required settings. No passwords checked here.

--- SciAuthZ/settings.py
+++ SciAuthZ/settings.py
@@ -86,25 +86,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',
                                    'rest_framework.permissions.DjangoModelPermissions'),
