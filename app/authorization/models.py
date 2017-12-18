7e555ea66e69ae391cec3425326ea204b400ed30
Nate Bessa <natebessa@gmail.com>
2017-12-18 15:34:38 -0500

    Removes all references to projects in exchange for a more generic "item". Also removes DUAs.
    The concept of projects has been moved out of SciAuthZ and into Hypatio. In its place, SciAuthZ now will track permissions that users have on an 'item', which is a generic text field. We will leave it to the external applications (i.e. hypatio) to decide how to name those items.

--- app/authorization/models.py
+++ app/authorization/models.py
@@ -8,33 +8,20 @@ from django.conf import settings
 import logging
 logger = logging.getLogger(__name__)
 
-class AuthorizableProject(models.Model):
-    """
-    An Authorizable project is the representation of a project that has the definitions for the projects privacy.
-    """
-    name = models.CharField(max_length=100, blank=False, null=False, verbose_name="Project Name")
-    project_key = models.CharField(max_length=100, blank=False, null=False, verbose_name="Project Key")
-    permission_scheme = models.CharField(max_length=100, default="PRIVATE", verbose_name="Permission Scheme")
-    dua_required = models.BooleanField(default=True)
-
-    def __str__(self):
-        return '%s %s' % (self.project_key, self.name)
-
-
 class UserPermissionRequest(models.Model):
     """
     This represents a user requesting permission to a specific project.
     """
     user = models.ForeignKey(User)
-    project = models.ForeignKey(AuthorizableProject)
+    item = models.CharField(max_length=100, blank=False, null=False, verbose_name="Item")
     date_requested = models.DateTimeField(auto_now_add=True)
     request_granted = models.BooleanField(default=False)
     date_request_granted = models.DateTimeField(default=None, null=True)
 
     def __str__(self):
-        return '%s %s %s' % (self.user, self.project, self.request_granted)
+        return '%s %s %s' % (self.user, self.item, self.request_granted)
 
-# Commented out until project manager information is stabilized
+# TODO Commented out until project manager information is stabilized
 # def handle_user_permission_request_added(instance, **kwargs):
 #     """
 #     When a new UserPermissionRequest is added, send an email to that project's project manager
@@ -72,39 +59,8 @@ class UserPermission(models.Model):
     This is the granting of permission to a user for a specific project.
     """
     user = models.ForeignKey(User)
-    project = models.ForeignKey(AuthorizableProject)
+    item = models.CharField(max_length=100, blank=False, null=False, verbose_name="Item")
     permission = models.CharField(max_length=100, blank=False, null=False, verbose_name="Permission")
 
     def __str__(self):
-        return '%s %s %s' % (self.user, self.project, self.permission)
-
-
-class DataUseAgreement(models.Model):
-    """
-    This is the text for the data use agreement associated with a project. The agreement_form field should be the name of the html file (with extension)
-    that contains the form. This file should be in the templates/duaforms/ directory. If no form is being used, leave the field blank and instead 
-    enter some text in the the agreement_text field.
-    """
-    name = models.CharField(max_length=100, blank=False, null=False, verbose_name="name")
-    project = models.ForeignKey(AuthorizableProject, related_name='duas')
-    agreement_text = models.TextField(blank=True)
-    agreement_form_file = models.CharField(max_length=50, blank=True)
-
-    def __str__(self):
-        return '%s %s' % (self.id, self.name)
-    
-    def clean(self):
-        if self.agreement_text != "" and self.agreement_form_file != "":
-            raise ValidationError('Cannot have both an agreement_text and agreement_form_file. Please choose one.')
-        if self.agreement_text == "" and self.agreement_form_file == "":
-            raise ValidationError('Either the agreement_text or agreement_form_file must have a value.')
-
-
-class DataUseAgreementSign(models.Model):
-    """
-    This represents a user signing a DUA for a specific project.
-    """
-    data_use_agreement = models.ForeignKey(DataUseAgreement)
-    user = models.ForeignKey(User)
-    date_signed = models.DateTimeField(auto_now_add=True)
-    agreement_text = models.TextField(blank=False)
+        return '%s %s %s' % (self.user, self.item, self.permission)
