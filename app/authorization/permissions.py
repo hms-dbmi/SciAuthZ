50a1a1e9cb24ce215731a46ad03753b8d56e4282
Nate Bessa <natebessa@gmail.com>
2018-08-24 15:49:53 -0400

    TC-237: Remove users from SciAuthZ and starts to remove UserPermissionRequest references
    Django users are not necessary in SciAuthZ. This commit includes a set of migrations that will move all the User's emails from the UserPermission model to a new "user_email" charfield. Also, this commit starts to remove usages of UserPermissionRequest which do not make sense to store in SciAuthZ. Lastly, this includes an update to the py-auth0-jwt-rest package. The new version of this package will no longer create users if they do not exist. Commented out tests.py which have long now been broken.

--- app/authorization/permissions.py
+++ app/authorization/permissions.py
@@ -5,38 +5,3 @@ from .models import UserPermission
 import logging
 logger = logging.getLogger(__name__)
 
-class IsAssociatedUser(permissions.BasePermission):
-    """
-    Custom permission to only allow users associated with an object or users with "MANAGER" permissions to edit it.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        # Is the user associated with the object the same as the user in the request.
-        if obj.user == request.user:
-            return True
-        # Check to see if the user has "MANAGE" permissions on this kind of item
-        else:
-            if UserPermission.objects.filter(item=obj.item, user=request.user, permission="MANAGE").count() >= 1:
-                return True
-            else:
-                return False
-
-
-def jwt_get_username_from_payload(payload):
-    """
-    Method to create user in SciAuthZ Application if they don't exist.
-    """
-
-    logger.debug("[SCIAUTHZ][DEBUG] jwt_get_username_from_payload: attempting to authenticate user - " + payload.get('email'))
-
-    try:
-        User.objects.get(username=payload.get('email'))
-    except User.DoesNotExist:
-
-        logger.debug("[SCIAUTHZ][DEBUG] jwt_get_username_from_payload: User not found, creating.")
-
-        user = User(username=payload.get('email'), email=payload.get('email'))
-        user.save()
-
-    return payload.get('email')
-
